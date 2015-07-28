a44a36ce66a131bfac018ba6353236de5417b3d1
Edward Ma <etduwx@gmail.com>
2015-07-27 17:56:13 -0700

    Can invoke without FROM

--- R/src-vertica.R
+++ R/src-vertica.R
@@ -481,11 +481,35 @@ db_explain.VerticaConnection <- function(con, sql, ...) {
   output[1:(graphVizInd-4)]
 }
 
+## Used to select UDxes without FROM clauses
+#' @export
+select <- function(arg,...) {
+
+  if(!is(arg,"tbl")) {
+      stopifnot(is(arg,"src_vertica"))
+      tbl <- make_tbl(c("vertica", "sql"),
+      src = arg,              # src object
+      from = NULL,            # table, join, or raw sql
+      select = NULL,          # SELECT: list of symbols
+      summarise = FALSE,      #   interpret select as aggreagte functions?
+      mutate = FALSE,         #   do select vars include new variables?
+      where = NULL,           # WHERE: list of calls
+      group_by = NULL,        # GROUP_BY: list of names
+      order_by = NULL         # ORDER_BY: list of calls
+    )
+    
+    mutate(tbl,...) 
+
+  } else {
+    dplyr::select(arg,...)
+  }
+
+}
+
 #' @export
 sql_escape_ident.VerticaConnection <- function(con, x) {
   sql_quote(x,' ')
 }
-
 
 # Analyze for performance
 #' @export
@@ -515,8 +539,62 @@ get_data_type <- function(val, ...) {
 
 #' @export
 sql_set_op.VerticaConnection <- dplyr:::sql_set_op.DBIConnection
+
+# Override allows SELECT without FROM
 #' @export
-sql_select.VerticaConnection <- dplyr:::sql_select.DBIConnection
+sql_select.VerticaConnection <- function(con, select, from, where = NULL,
+                                     group_by = NULL, having = NULL,
+                                     order_by = NULL, limit = NULL,
+                                     offset = NULL, ...) {
+
+  out <- vector("list", 8)
+  names(out) <- c("select", "from", "where", "group_by", "having", "order_by",
+    "limit", "offset")
+
+  assert_that(is.character(select), length(select) > 0L)
+  out$select <- build_sql("SELECT ", escape(select, collapse = ", ", con = con))
+
+  if (length(from) > 0L) { 
+    assert_that(is.character(from))
+    out$from <- build_sql("FROM ", from, con = con)
+  }
+
+  if (length(where) > 0L) {
+    assert_that(is.character(where))
+    out$where <- build_sql("WHERE ",
+      escape(where, collapse = " AND ", con = con))
+  }
+
+  if (!is.null(group_by)) {
+    assert_that(is.character(group_by), length(group_by) > 0L)
+    out$group_by <- build_sql("GROUP BY ",
+      escape(group_by, collapse = ", ", con = con))
+  }
+if (!is.null(having)) {
+    assert_that(is.character(having), length(having) == 1L)
+    out$having <- build_sql("HAVING ",
+      escape(having, collapse = ", ", con = con))
+  }
+
+  if (!is.null(order_by)) {
+    assert_that(is.character(order_by), length(order_by) > 0L)
+    out$order_by <- build_sql("ORDER BY ",
+      escape(order_by, collapse = ", ", con = con))
+  }
+
+  if (!is.null(limit)) {
+    assert_that(is.integer(limit), length(limit) == 1L)
+    out$limit <- build_sql("LIMIT ", limit, con = con)
+  }
+
+  if (!is.null(offset)) {
+    assert_that(is.integer(offset), length(offset) == 1L)
+    out$offset <- build_sql("OFFSET ", offset, con = con)
+  }
+
+  escape(unname(dplyr:::compact(out)), collapse = "\n", parens = FALSE, con = con)
+}
+
 #' @export
 sql_escape_string.VerticaConnection <- dplyr:::sql_escape_string.DBIConnection
 #' @export
